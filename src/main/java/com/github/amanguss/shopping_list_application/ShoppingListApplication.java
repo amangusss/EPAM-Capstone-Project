b5c9c85b0b4ae477025256de1e2e4e7fbf8fd83f
amangusss <aman.nazarkulov@gmail.com>
2025-07-22 16:22:34 +0600

    Implementation: Initial Spring Boot project

---
+++ src/main/java/com/github/amanguss/shopping_list_application/ShoppingListApplication.java
@@ -0,0 +1,13 @@
+package com.github.amanguss.shopping_list_application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListApplication.class, args);
+	}
+
+}
