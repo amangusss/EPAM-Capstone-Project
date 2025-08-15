710d5b1a80e9fd6e79d4e862e54445848edbb691
amangusss <aman.nazarkulov@gmail.com>
2025-08-15 23:40:46 +0600

    Implementation|Back-end: Exception handling

---
+++ src/main/java/com/github/amanguss/shopping_list_application/exception/ValidationException.java
@@ -0,0 +1,12 @@
+package com.github.amanguss.shopping_list_application.exception;
+
+public class ValidationException extends RuntimeException {
+
+    public ValidationException(String message) {
+        super(message);
+    }
+
+    public ValidationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
